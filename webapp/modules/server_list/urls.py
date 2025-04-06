aac870ddb90c8f27a8ebb66165c5f41f845f2065
Rafli <raflisatriandi@outlook.co.id>
2025-04-06 17:42:00 +0700

    DONE add and delete server management

--- webapp/modules/server_list/urls.py
+++ webapp/modules/server_list/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import ServerManagerView
+from .views import ServerManagerView, ServerManagerDetailView
 
 urlpatterns = [
     path('', ServerManagerView.as_view(), name="server_list"),
+    path('<int:id>', ServerManagerDetailView.as_view(), name="server_details"),
 ]
--- webapp/modules/server_list/views.py
+++ webapp/modules/server_list/views.py
@@ -15,6 +15,7 @@ class ServerManagerView(View):
         server_detail_list = []
         for server in server_list:
             server_detail = {
+                "id": server.pk,
                 "name": server.server_name,
                 "url": server.server_url,
                 "active": server.is_active
@@ -37,13 +38,71 @@ class ServerManagerView(View):
         server_url = req_body.get("server-address")
         server_name = req_body.get("server-name")
 
-        server_list = ServerList(
-            server_url = server_url,
-            server_name = server_name,
-            is_active = True,
-            created_by = req_user,
-            updated_by = req_user,
-        )
-        server_list.save()
+        try:
+
+            server_list = ServerList(
+                server_url = server_url,
+                server_name = server_name,
+                is_active = True,
+                created_by = req_user,
+                updated_by = req_user,
+            )
+            server_list.save()
+
+        except Exception as err:
+            print(f"Error when storing server: {err.args}")
 
         return redirect("server_list")
+    
+class ServerManagerDetailView(View):
+    template = "modules/server_list/templates/index.html"
+    ctx = {
+        "page_title": "server_manager"
+    }
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+
+        server_id = kwargs.get("id")
+        resp_data = {}
+
+        try:
+            if not server_id:
+                raise Exception("server_id is empty")
+            
+            server_details = ServerList.objects.filter(pk=server_id).first()
+            if not server_details:
+                raise Exception("server_details is empty")
+                
+            resp_data["id"] = server_details.pk
+            resp_data["name"] = server_details.server_name
+            resp_data["url"] = server_details.server_url
+            resp_data["active"] = server_details.is_active
+            resp_data["players"] = 0
+
+            return JsonResponse(resp_data)
+
+        except Exception as err:
+            return JsonResponse(resp_data)
+        
+    def delete(self, request: HttpRequest, *args, **kwargs):
+
+        server_id = kwargs.get("id")
+        resp_data = {}
+
+        try:
+            if not server_id:
+                raise Exception("server_id is empty")
+            
+            server_details = ServerList.objects.filter(pk=server_id).first()
+            if not server_details:
+                raise Exception("server_details is empty")
+                
+            server_details.delete()
+
+            resp_data["deleted"] = True
+
+            return JsonResponse(resp_data)
+
+        except Exception as err:
+            resp_data["deleted"] = False
+            return JsonResponse(resp_data)
