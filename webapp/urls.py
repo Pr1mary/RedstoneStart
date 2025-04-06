2cf255b9c27fce65fbf4b101fbef9fc35dfb96cc
Rafli <raflisatriandi@outlook.co.id>
2025-04-07 01:18:57 +0700

    DONE add main page redirect

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include("webapp.modules.main_page.urls"), name="main_page"),
     path('servermanager/', include("webapp.modules.server_list.urls"), name="server_manager"),
     path('playermanager/', include("webapp.modules.player_list.urls"), name="player_manager")
 ]
