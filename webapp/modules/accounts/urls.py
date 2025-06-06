fe0c4aeaf8660c62cfcac3dffd1497a3a21c6ae5
Rafli <raflisatriandi@outlook.co.id>
2025-06-07 01:14:59 +0700

    DONE user details info and update

--- webapp/modules/accounts/urls.py
+++ webapp/modules/accounts/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import UserLoginView, UserLogoutView, UserRegisterView
+from .views import UserLoginView, UserLogoutView, UserRegisterView, UserInfoDetailsView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('auth/login', UserLoginView.as_view(), name="auth_login"),
     path('auth/register', UserRegisterView.as_view(), name="auth_register"),
     path('auth/logout', UserLogoutView.as_view(), name="auth_logout"),
+    path('details', UserInfoDetailsView.as_view(), name="details_info"),
 ]
--- webapp/modules/accounts/views.py
+++ webapp/modules/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse, HttpRequest, HttpResponseRedirect
 from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
@@ -74,4 +75,44 @@ class UserLogoutView(View):
             logout(request)
 
         return redirect("auth_login")
+    
+class UserInfoDetailsView(LoginRequiredMixin, View):
+    template = "modules/accounts/templates/index.html"
+    ctx = {
+        "page_title": "register_page"
+    }
+    login_url = "/accounts/auth/login"
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            self.ctx["curr_username"] = request.user.username
+            self.ctx["curr_email"] = request.user.email
+
+        return render(request, self.template, self.ctx)
+    
+    def post(self, request: HttpRequest, *args, **kwargs):
+
+        data = request.POST
+
+        try:
+            if not request.user.is_authenticated:
+                raise Exception("not authenticated")
+            
+            if data.get("email"):
+                request.user.email = data.get("email")
+
+            if data.get("password"):
+                request.user.set_password(data.get("password"))
+
+            request.user.save()
+
+        except Exception as e:
+            if e.args[0] == "not authenticated":
+                messages.error(request, "User is not authenticated")
+            else:
+                messages.error(request, "Unknown error when updating user data")
+            return redirect("details_info")
         
+        messages.info(request, "User data successfully updated! Please login again.")
+        return redirect("details_info")
