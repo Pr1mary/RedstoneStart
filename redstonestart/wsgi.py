4a1561fe2528d97001be27310d70f47df3ef3739
pr1mary <raflisatriandi@outlook.co.id>
2026-01-03 02:24:16 +0700

    DONE rename main app structure to redstonestart

--- redstonestart/wsgi.py
+++ redstonestart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mcplayer_manager project.
+WSGI config for redstonestart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcplayer_manager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redstonestart.settings')
 
 application = get_wsgi_application()
