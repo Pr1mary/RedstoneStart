5c4610fa0779af2ae1ed263a33ad38d5d6866ada
Rafli <raflisatriandi@outlook.co.id>
2026-09-14 00:24:13 +0700

    DONE new invite form redesign

--- webapp/modules/player_list/urls.py
+++ webapp/modules/player_list/urls.py
@@ -4,12 +4,14 @@ from .views import (
     PlayerManagerView, PlayerManagerDetailView,
     PlayerJoinManagerView, PlayerBanManagerView,
     PlayerUnbanManagerView, PlayerSyncManagerView,
+    PlayerJoinManagerSuccessView
     )
 
 urlpatterns = [
     path('', PlayerManagerView.as_view(), name="player_list"),
     path('<int:id>', PlayerManagerDetailView.as_view(), name="player_details"),
     path('joinserver', PlayerJoinManagerView.as_view(), name="player_join_server"),
+    path('joinserver/success', PlayerJoinManagerSuccessView.as_view(), name="player_join_server_success"),
     path('ban', PlayerBanManagerView.as_view(), name="player_server_ban"),
     path('unban', PlayerUnbanManagerView.as_view(), name="player_server_unban"),
     path('sync', PlayerSyncManagerView.as_view(), name="player_server_sync"),
