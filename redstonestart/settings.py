9cc6a3fc9b74f8e05c5c9d6d674cc6c388daa6fa
Rafli <raflisatriandi@outlook.co.id>
2026-09-15 00:05:53 +0700

    WIP change smtp support from anymail to native django

--- redstonestart/settings.py
+++ redstonestart/settings.py
@@ -42,11 +42,16 @@ else:
     CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Email definition
-ANYMAIL = {
-  "MAILTRAP_API_TOKEN": os.getenv("MAILTRAP_API_TOKEN", ""),
-}
-EMAIL_BACKEND = "anymail.backends.mailtrap.EmailBackend"
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
+EMAIL_HOST=os.getenv("EMAIL_HOST", "")
+EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_PORT=os.getenv("EMAIL_PORT", "")
+EMAIL_USE_TLS=os.getenv("EMAIL_USE_TLS", "").lower() == "true"
+# ANYMAIL = {
+#   "MAILTRAP_API_TOKEN": os.getenv("MAILTRAP_API_TOKEN", ""),
+# }
+# EMAIL_BACKEND = "anymail.backends.mailtrap.EmailBackend"
+# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
 
 # Application definition
 
