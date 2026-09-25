02a2bb1e14d79c158ab0ee5dd25accd3725ccab2
Rafli <raflisatriandi@outlook.co.id>
2026-09-26 01:32:15 +0700

    DONE working on forgot password email template

--- webapp/modules/accounts/urls.py
+++ webapp/modules/accounts/urls.py
@@ -6,13 +6,19 @@ from django.contrib.auth.views import (
     PasswordResetConfirmView, PasswordResetCompleteView
 )
 
+from django.conf import settings
+
 urlpatterns = [
     path('auth/login', UserLoginView.as_view(), name="auth_login"),
     path('auth/register', UserRegisterView.as_view(), name="auth_register"),
     path(
       'auth/reset',
-      PasswordResetView.as_view(template_name="modules/accounts/templates/index-reset.html"),
-      name="password_reset"),
+      PasswordResetView.as_view(
+        template_name="modules/accounts/templates/index-reset.html",
+        html_email_template_name="modules/accounts/templates/index-reset-email.html",
+        email_template_name="modules/accounts/templates/index-reset-email-text.html",
+        subject_template_name="modules/accounts/templates/index-reset-email-subject.txt",
+        ), name="password_reset"),
     path(
       'auth/reset/done',
       PasswordResetDoneView.as_view(template_name="modules/accounts/templates/index-reset-requested.html"),
